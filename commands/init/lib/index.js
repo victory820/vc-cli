6add29a83cce1ffd46b9353dc14e718201008d98
victor <sheng4526160@163.com>
2024-07-01 16:44:08 +0800

    发布新内容

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 function init(projectName, options) {
-  console.log('init=========', projectName, options, process.env.CLI_TARGET_PATH)
+  // 可接受自定义
+  console.log('init command test', projectName, options, process.env.CLI_TARGET_PATH)
 }
 
 module.exports = init
