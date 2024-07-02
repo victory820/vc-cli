e3fe2b339ea67273cd36089393b5b071bb888e54
victor <sheng4526160@163.com>
2024-07-02 18:56:49 +0800

    清空目录提示

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,10 @@
 'use strict'
 
+const fs = require('node:fs')
+
+const { confirm } = require('@inquirer/prompts')
+const fse = require('fs-extra')
+
 const Command = require('@vc-cli/command')
 const log = require('@vc-cli/log')
 
@@ -11,8 +16,46 @@ class InitCommand extends Command {
     log.verbose('projectName', this.projectName)
     log.verbose('force', this.force)
   }
-  exec() {
-    console.log('init业务逻辑')
+  async exec() {
+    try {
+      // 准备阶段
+      await this.prepare()
+      // 下载模版
+      // 模板安装
+    } catch (error) {
+      log.error(error.message)
+    }
+  }
+
+  async prepare() {
+    // 当前目录是否为空
+    const cwdPath = process.cwd()
+    if (!this.isDirEmpty(cwdPath)) {
+      // 如果目录下有内容
+      let isContinue = true
+      if (!this.force) {
+        // 非强制处理，才询问
+        isContinue = await confirm({ message: '当前目录不为空是否继续创建项目？', default: false })
+      }
+      if (isContinue) {
+        const isDelete = await confirm({
+          message: '是否删除当前文件夹下所有文件？',
+          default: false
+        })
+        if (isDelete) {
+          console.log('删除所有')
+          fse.emptyDirSync(cwdPath)
+        }
+      }
+    }
+  }
+  isDirEmpty(cwdPath) {
+    let fileList = fs.readdirSync(cwdPath)
+    // 文件过滤
+    fileList = fileList.filter((file) => {
+      return !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+    })
+    return !fileList || fileList.length === 0
   }
 }
 
